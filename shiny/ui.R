af7a7b008900f398b5b2c507da809977f5d810a0
Graham Clenaghan <clenagh@math.washington.edu>
2015-04-27 20:57:37 -0700

    use smaller step for precipitation slider

--- shiny/ui.R
+++ shiny/ui.R
@@ -8,7 +8,7 @@ fluidPage(
   sidebarPanel(
     radioButtons("weekday", NULL, c("All days", "Weekdays", "Weekends")),
     sliderInput("temp", "High Temperature", 30, 95, c(30,95), step=1, ticks=TRUE),
-    sliderInput("precip", "Total Precipitation", 0, 2.49, c(0, 2.49), ticks=TRUE),
+    sliderInput("precip", "Total Precipitation", 0, 2.49, c(0, 2.49), step=.01, ticks=TRUE),
     textOutput("numdays")
   ),
   
