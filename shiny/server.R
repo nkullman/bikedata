907ea3965e25dfa710b9750312444066838837b1
Graham Clenaghan <clenagh@math.washington.edu>
2015-04-27 20:22:51 -0700

    add some formatting and a temperature slider

--- shiny/server.R
+++ shiny/server.R
@@ -16,14 +16,19 @@ function(input, output) {
     } else {
       df = fremont
     }
+    df = df[input$temp[1] <= df$Max_TemperatureF & df$Max_TemperatureF <= input$temp[2],]
     df %>% group_by(Time, Direction) %>% summarize(CyclistCount=mean(CyclistCount))
     })
   
     
    p <- reactive({dataset %>% 
                     ggvis(x=~Time, y=~CyclistCount) %>% 
-                    group_by(Direction) %>% 
-                    layer_lines(stroke=~Direction)
+                    group_by(Direction) %>%
+                    add_axis("x", properties = axis_props(labels=list(angle=30, align="left", fontSize=12)),
+                             title_offset=60) %>%
+                    add_axis("y", title="Average Bicycle Count", title_offset=50) %>%
+                    layer_lines(stroke=~Direction, strokeWidth := 2) %>%
+                    scale_numeric("y", domain=c(0, 550))
    })
     
    p %>% bind_shiny("plot")
--- shiny/ui.R
+++ shiny/ui.R
@@ -6,7 +6,8 @@ fluidPage(
   titlePanel("Fremont Bridge Bicycle Counts"),
   
   sidebarPanel(
-    radioButtons("weekday", NULL, c("All days", "Weekdays", "Weekends"))
+    radioButtons("weekday", NULL, c("All days", "Weekdays", "Weekends")),
+    sliderInput("temp", "High Temperature", 30, 95, c(30,95), step=1, ticks=TRUE, animate=animationOptions(interval=250))
   ),
   
   mainPanel(
